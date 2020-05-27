fe38109e6c47429de9ce5f6e90c3de85cdc4a77f
Sylvain Bertrand <sbertrand@ihmc.us>
2020-05-27 16:34:17 -0500

    :arrow_up: ihmc-graphics-description 0.17.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ mainDependencies {
    api("us.ihmc:ihmc-pub-sub:0.12.0")
    api("us.ihmc:ihmc-pub-sub-serializers-extra:0.12.0")
    api("us.ihmc:ihmc-commons:0.29.0")
-   api("us.ihmc:ihmc-graphics-description:0.16.0")
+   api("us.ihmc:ihmc-graphics-description:0.17.0")
    api("us.ihmc:mecano:0.4.1")
 }
 
