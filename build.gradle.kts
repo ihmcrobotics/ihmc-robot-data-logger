7a495766edc6d5d378307289e20ee73d302f4774
Sylvain Bertrand <sbertrand@ihmc.us>
2020-06-18 10:55:54 -0500

    :arrow_up: ihmc-yovariables 0.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ mainDependencies {
    api("org.openjdk.jol:jol-core:0.9")
 
    api("us.ihmc:euclid:0.15.0")
-   api("us.ihmc:ihmc-yovariables:0.7.0-ejml-0.39-beta-1")
+   api("us.ihmc:ihmc-yovariables:0.8.0")
    api("us.ihmc:ihmc-video-codecs:2.1.5")
    api("us.ihmc:ihmc-realtime:1.3.0")
    api("us.ihmc:ihmc-java-decklink-capture:0.3.3")
