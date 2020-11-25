3f8ad127c252437f1f5c927a4f1f797ea491cd32
Duncan Calvert <dcalvert@ihmc.us>
2020-11-24 22:47:24 -0600

    :arrow_up: ihmc-commons 0.30.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,13 +45,13 @@ mainDependencies {
    api("us.ihmc:ihmc-java-decklink-capture:0.3.3")
    api("us.ihmc:ihmc-pub-sub:0.15.0")
    api("us.ihmc:ihmc-pub-sub-serializers-extra:0.15.0")
-   api("us.ihmc:ihmc-commons:0.30.2")
+   api("us.ihmc:ihmc-commons:0.30.4")
    api("us.ihmc:ihmc-graphics-description:0.19.1")
    api("us.ihmc:mecano:0.7.1")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.30.2")
+   api("us.ihmc:ihmc-commons-testing:0.30.4")
 }
 
 app.entrypoint("IHMCLogger", "us.ihmc.robotDataLogger.logger.YoVariableLoggerDispatcher")
