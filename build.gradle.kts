80d505c948b08ba3606d5f70b0ecd26adde3e0fc
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-02 11:13:52 -0600

    :arrow_up: mecano 0.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ mainDependencies {
    api("us.ihmc:ihmc-pub-sub-serializers-extra:0.15.0")
    api("us.ihmc:ihmc-commons:0.30.4")
    api("us.ihmc:ihmc-graphics-description:0.19.1")
-   api("us.ihmc:mecano:0.7.1")
+   api("us.ihmc:mecano:0.7.2")
 }
 
 testDependencies {
