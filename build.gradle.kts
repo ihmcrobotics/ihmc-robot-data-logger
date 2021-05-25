d91f63bab018a9c4bca7c70f2f7f92271e093879
Duncan Calvert <dcalvert@ihmc.us>
2021-05-25 09:35:32 -0500

    :bookmark: 0.20.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.20.7"
+   version = "0.20.8"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
