c0d05f3933dcbaa6cb5a39acc86240980a989d05
Jesper Smith <jesper@halodi.com>
2022-06-15 13:51:36 +0200

    Updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.23.2"
+   version = "0.24.0-halodi1"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
