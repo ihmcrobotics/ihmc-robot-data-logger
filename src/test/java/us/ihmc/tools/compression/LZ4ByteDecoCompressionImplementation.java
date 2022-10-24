6bc1a49fb12d9528ab69f29cdbf53d562a698a99
nkitchel <nkitchel@ihmc.org>
2022-10-24 11:47:49 -0500

    Added LZ4 1.9 compression implementation for compress and decompress methods.

---
+++ src/test/java/us/ihmc/tools/compression/LZ4ByteDecoCompressionImplementation.java
@@ -0,0 +1,63 @@
+package us.ihmc.tools.compression;
+
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import org.bytedeco.javacpp.*;
+import org.bytedeco.lz4.*;
+import org.bytedeco.lz4.global.lz4;
+
+public class LZ4ByteDecoCompressionImplementation
+{
+   public static LZ4FDecompressionContext ByteDecoLZ4CompressionImplementation() throws LZ4Exception
+   {
+            final LZ4FDecompressionContext dctx = new LZ4FDecompressionContext();
+            final long ctxError = lz4.LZ4F_createDecompressionContext(dctx, lz4.LZ4F_VERSION);
+            checkForError(ctxError);
+            return dctx;
+   }
+
+   public static void compress(ByteBuffer src, Pointer srcPointer, ByteBuffer dst, Pointer dstPointer)
+   {
+      lz4.LZ4F_compressFrame(dstPointer, dst.limit(), srcPointer, src.limit(), null);
+   }
+
+   public static void decompress(LZ4FDecompressionContext dctx,
+                                 Pointer compressedPointer,
+                                 Pointer dstPointer,
+                                 SizeTPointer srcSize,
+                                 SizeTPointer dstSize,
+                                 ByteBuffer compressed,
+                                 int uncompressedSize)
+   {
+      if (compressed.position() + uncompressedSize > compressed.limit())
+      {
+         throw new BufferOverflowException();
+      }
+
+      lz4.LZ4F_decompress(dctx, dstPointer, dstSize, compressedPointer, srcSize, null);
+   }
+
+   public int maxCompressedLength(int uncompressedLength)
+   {
+      final int maxCompressedSize = (int) lz4.LZ4F_compressFrameBound(uncompressedLength, null);
+      return lz4.LZ4_compressBound(maxCompressedSize);
+   }
+
+   public int minimumDecompressedLength(int compressedLength)
+   {
+      double y = (long) (compressedLength - 16) * 255;
+      return (int) Math.round(y / 256);
+   }
+
+   private static void checkForError(long errorCode) throws LZ4Exception {
+      if (lz4.LZ4F_isError(errorCode) != 0) {
+         throw new LZ4Exception(lz4.LZ4F_getErrorName(errorCode).getString());
+      }
+   }
+
+   public static final class LZ4Exception extends Exception {
+      public LZ4Exception(final String message) {
+         super(message);
+      }
+   }
+}
