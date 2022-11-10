787fee53057a0b6e5ed72fece2b50ee94ebdfcfd
nkitchel <nkitchel@ihmc.org>
2022-11-10 10:47:07 -0600

    Fixed formatting mistakes, added javadoc comments for readability

--- src/test/java/us/ihmc/tools/compression/LZ4BytedecoCompressionImplementation.java
+++ src/test/java/us/ihmc/tools/compression/LZ4BytedecoCompressionImplementation.java
@@ -10,7 +10,7 @@ public class LZ4BytedecoCompressionImplementation
 {
    final static LZ4FDecompressionContext lz4DecompressionContext = new LZ4FDecompressionContext();
 
-   // Method checks to see if anything goes wrong with the decompression of the data
+   /** Method checks to see if anything goes wrong with the decompression of the data */
    public static LZ4FDecompressionContext BytedecoLZ4CompressionImplementation() throws LZ4Exception
    {
             final long contextError = lz4.LZ4F_createDecompressionContext(lz4DecompressionContext, lz4.LZ4F_VERSION);
@@ -18,13 +18,13 @@ public class LZ4BytedecoCompressionImplementation
             return lz4DecompressionContext;
    }
 
-   // Compresses the data using pointers that point to the addresses of the ByteBuffers
+   /** Compresses the data using pointers that point to the addresses of the ByteBuffers */
    public static double compress(ByteBuffer source, Pointer sourcePointer, ByteBuffer destination, Pointer destionationPointer)
    {
       return lz4.LZ4F_compressFrame(destionationPointer, destination.limit(), sourcePointer, source.limit(), null);
    }
 
-   // Decompresses the data using several pointers that point to the ByteBuffers
+   /** Decompresses the data using several pointers that point to the ByteBuffers */
    public static void decompress(LZ4FDecompressionContext decompressionContext,
                                  Pointer compressedPointer,
                                  Pointer destinationPointer,
@@ -41,28 +41,30 @@ public class LZ4BytedecoCompressionImplementation
       lz4.LZ4F_decompress(decompressionContext, destinationPointer, destinationSize, compressedPointer, compressSize, null);
    }
 
-   // Computes the amount of space required for the compress Buffer based on the size of the input data
+   /** Computes the amount of space required for the compress Buffer based on the size of the input data */
    public int maxCompressedLength(int uncompressedLength)
    {
       final int maxCompressedSize = (int) lz4.LZ4F_compressFrameBound(uncompressedLength, null);
       return lz4.LZ4_compressBound(maxCompressedSize);
    }
 
-   // Computes the amount of space required for the decompress Buffer bases on the size of the compressed data
+   /** Computes the amount of space required for the decompress Buffer bases on the size of the compressed data */
    public int minimumDecompressedLength(int compressedLength)
    {
       double y = (long) (compressedLength - 16) * 255;
       return (int) Math.round(y / 256);
    }
 
-   // Checks for errors with the decompress method and throw's an exception from LZ4Exception if one is found
-   private static void checkForError(long errorCode) throws LZ4Exception {
+   /** Checks for errors with the decompress method and throw's an exception from LZ4Exception if one is found */
+   private static void checkForError(long errorCode) throws LZ4Exception
+   {
       if (lz4.LZ4F_isError(errorCode) != 0) {
          throw new LZ4Exception(lz4.LZ4F_getErrorName(errorCode).getString());
       }
    }
 
-   public static final class LZ4Exception extends Exception {
+   public static final class LZ4Exception extends Exception
+   {
       public LZ4Exception(final String message) {
          super(message);
       }
