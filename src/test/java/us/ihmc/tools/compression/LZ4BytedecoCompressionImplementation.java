3e48f1fbf66512cd8a5f3d648a420bafb9d1a805
nkitchel <nkitchel@ihmc.org>
2022-11-10 09:57:22 -0600

    Changed to full name variables

--- src/test/java/us/ihmc/tools/compression/LZ4BytedecoCompressionImplementation.java
+++ src/test/java/us/ihmc/tools/compression/LZ4BytedecoCompressionImplementation.java
@@ -8,28 +8,28 @@ import org.bytedeco.lz4.global.lz4;
 
 public class LZ4BytedecoCompressionImplementation
 {
-   final static LZ4FDecompressionContext dctx = new LZ4FDecompressionContext();
+   final static LZ4FDecompressionContext lz4DecompressionContext = new LZ4FDecompressionContext();
 
-   // Method checks to see if anything goes wrong with the decomopression of the data
+   // Method checks to see if anything goes wrong with the decompression of the data
    public static LZ4FDecompressionContext BytedecoLZ4CompressionImplementation() throws LZ4Exception
    {
-            final long ctxError = lz4.LZ4F_createDecompressionContext(dctx, lz4.LZ4F_VERSION);
-            checkForError(ctxError);
-            return dctx;
+            final long contextError = lz4.LZ4F_createDecompressionContext(lz4DecompressionContext, lz4.LZ4F_VERSION);
+            checkForError(contextError);
+            return lz4DecompressionContext;
    }
 
    // Compresses the data using pointers that point to the addresses of the ByteBuffers
-   public static double compress(ByteBuffer src, Pointer srcPointer, ByteBuffer dst, Pointer dstPointer)
+   public static double compress(ByteBuffer source, Pointer sourcePointer, ByteBuffer destination, Pointer destionationPointer)
    {
-      return lz4.LZ4F_compressFrame(dstPointer, dst.limit(), srcPointer, src.limit(), null);
+      return lz4.LZ4F_compressFrame(destionationPointer, destination.limit(), sourcePointer, source.limit(), null);
    }
 
    // Decompresses the data using several pointers that point to the ByteBuffers
-   public static void decompress(LZ4FDecompressionContext dctx,
+   public static void decompress(LZ4FDecompressionContext decompressionContext,
                                  Pointer compressedPointer,
-                                 Pointer dstPointer,
-                                 SizeTPointer srcSize,
-                                 SizeTPointer dstSize,
+                                 Pointer destinationPointer,
+                                 SizeTPointer compressSize,
+                                 SizeTPointer destinationSize,
                                  ByteBuffer uncompressed,
                                  int uncompressedSize)
    {
@@ -38,7 +38,7 @@ public class LZ4BytedecoCompressionImplementation
          throw new BufferOverflowException();
       }
 
-      lz4.LZ4F_decompress(dctx, dstPointer, dstSize, compressedPointer, srcSize, null);
+      lz4.LZ4F_decompress(decompressionContext, destinationPointer, destinationSize, compressedPointer, compressSize, null);
    }
 
    // Computes the amount of space required for the compress Buffer based on the size of the input data
