fc810287ec78e688511a50bc33639f4cc37f0fd8
Nick Kitchel <nkitchel@ihmc.org>
2023-02-28 11:04:23 -0600

    Wroking GStreamer that records video which can be either an mov or an mp4. Runs for 10 seconds with no frame capture.

--- src/main/java/us/ihmc/publisher/logger/CameraGstreamer.java
+++ src/main/java/us/ihmc/publisher/logger/CameraGstreamer.java
@@ -2,6 +2,8 @@ package us.ihmc.publisher.logger;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 import org.freedesktop.gstreamer.*;
 import org.freedesktop.gstreamer.event.EOSEvent;
 import us.ihmc.commons.thread.ThreadTools;
@@ -17,9 +19,10 @@ public class CameraGstreamer
     private static StringBuffer videoCaps = new StringBuffer();
     private static StringBuffer audioCaps = new StringBuffer();
     private static Semaphore gotCaps = new Semaphore(2);
+    private static final Semaphore gotEOSPlayBin = new Semaphore(1);
     private static Semaphore canSend = new Semaphore(2);
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         Gst.init();
 
@@ -33,12 +36,39 @@ public class CameraGstreamer
                 "! videorate " +
                 "! x264enc " +
                 "! mp4mux " +
-                "! filesink location=/home/nick/xyz.mp4");
+                "! filesink location=/home/nick/xyz.mov");
+//                "! filesink location=/home/nick/xyz.mp4");
+
+        pipe.getBus().connect((Bus.EOS) (source) ->
+        {
+            System.out.println("Recieved the EOS on the pipeline!!!");
+            gotEOSPlayBin.release();
+        });
+
+        gotEOSPlayBin.drainPermits();
 
         pipe.play();
         ThreadTools.sleepSeconds(10);
+
         pipe.sendEvent(new EOSEvent());
+        gotEOSPlayBin.acquire(1);
+
+        System.out.println("Stopped Capture");
+
         pipe.stop();
+
+
+//        Gst.getExecutor().schedule(Gst::quit, 10, TimeUnit.SECONDS);
+
+
+
+
+//        Gst.main();
+
+//        pipe.sendEvent(new EOSEvent());
+//
+//        pipe.stop();
+//        ThreadTools.sleepSeconds(4);
 
         // Doesn't shut down the pipeline properly so the video is lost.
     }
