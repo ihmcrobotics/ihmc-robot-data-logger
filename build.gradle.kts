473fd02d6d3a829d89504837325e7cd9b0e07e48
Sylvain Bertrand <sbertrand@ihmc.org>
2023-03-10 10:50:39 -0600

    :arrow_up: scs2 17-0.13.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ mainDependencies {
    api("us.ihmc:ihmc-graphics-description:0.20.0")
    api("us.ihmc:mecano:17-0.11.6")
    api("com.hierynomus:sshj:0.31.0")
-   api("us.ihmc:scs2-definition:17-0.13.1")
+   api("us.ihmc:scs2-definition:17-0.13.2")
 
    var javaFXVersion = "17.0.2"
    api(ihmc.javaFXModule("base", javaFXVersion))
