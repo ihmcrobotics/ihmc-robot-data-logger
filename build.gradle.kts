ead9493796d9b520a0cadd73ff2ae8a6abe90048
Jesper Smith <jesper@halodi.com>
2023-04-13 16:18:17 +0200

    Remove halodi from version flag

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.28.0-halodi1"
+   version = "0.28.0"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
