698dbf7bc5240a016223f67f90cbb14c328fbcc9
Sylvain Bertrand <sbertrand@ihmc.org>
2023-06-27 18:04:25 -0500

    :bookmark: 0.28.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.28.1"
+   version = "0.28.2"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
