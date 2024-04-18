2788a05ffaad0395e0b8d5564144ac18b1be936d
Nick Kitchel <nkitchel@ihmc.org>
2024-04-17 21:02:43 -0500

    Added javadoc to DefaultLogModelProvider to try and be helpful in the future

--- src/main/java/us/ihmc/multicastLogDataProtocol/modelLoaders/DefaultLogModelProvider.java
+++ src/main/java/us/ihmc/multicastLogDataProtocol/modelLoaders/DefaultLogModelProvider.java
@@ -19,6 +19,17 @@ public class DefaultLogModelProvider<T> implements LogModelProvider
    private final Predicate<String> filter;
    private final String[] topLevelResourceDirectories;
 
+   /**
+    * This allows the key parts of a model to be stored and used later. Resources for that model are included
+    * in the log. The user is allowed to specify a filter to the resources in case they don't want every resource
+    * to be logged.
+    * @param modelLoader The type of model to be logged
+    * @param modelName The name of the model to be logged
+    * @param modelFileAsStream The stream of data that contains the model
+    * @param filter Can be null, if null, all resources are grabbed,
+    *               Otherwise only grab resources that match the filter
+    * @param topLevelResourceDirectories The resources to be logged
+    */
    public DefaultLogModelProvider(Class<T> modelLoader,
                                   String modelName,
                                   InputStream modelFileAsStream,
