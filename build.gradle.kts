267a9d3f826f2de70b8bd80c97331c385f1c1e99
nkitchel <nkitchel@ihmc.org>
2024-08-21 15:08:27 -0500

    Update bytedeco to 1.5.9 to match other imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,9 +54,9 @@ mainDependencies {
    api("org.bytedeco:javacv-platform:1.5.10")
    api("org.freedesktop.gstreamer:gst1-java-core:1.4.0")
 
-   api("org.bytedeco:javacv:1.5.10")
-   api("org.bytedeco:javacpp:1.5.10")
-   api("org.bytedeco:javacv-platform:1.5.10")
+   api("org.bytedeco:javacv:1.5.9")
+   api("org.bytedeco:javacpp:1.5.9")
+   api("org.bytedeco:javacv-platform:1.5.9")
    api("org.freedesktop.gstreamer:gst1-java-core:1.4.0")
 
    var javaFXVersion = "17.0.2"
