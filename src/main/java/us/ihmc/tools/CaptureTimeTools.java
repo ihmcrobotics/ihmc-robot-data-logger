c1190c167dd8b34a1ff849947e05cbc325891fb0
OperatorComputer <OperatorComputer@unknown>
2024-08-29 13:43:26 -0500

    Add docs to CaptureTimeTools

--- src/main/java/us/ihmc/tools/CaptureTimeTools.java
+++ src/main/java/us/ihmc/tools/CaptureTimeTools.java
@@ -2,8 +2,14 @@ package us.ihmc.tools;
 
 public class CaptureTimeTools
 {
-   public static long timeSinceStartedCaptureInSeconds(long milliseconds, long startTime)
+   /**
+    * This takes the current time in milliseconds, and returns the seconds that have passed from the start time.
+    * @param currentTimeInMilliSeconds is the current time that you want to compare too
+    * @param startTime is the start time from when capture started
+    * @return the time in seconds that has passed since the start time
+    */
+   public static long timeSinceStartedCaptureInSeconds(long currentTimeInMilliSeconds, long startTime)
    {
-      return 1000 * (milliseconds - startTime);
+      return 1000 * (currentTimeInMilliSeconds - startTime);
    }
 }
