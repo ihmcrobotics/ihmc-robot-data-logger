956491ab0a85b86d0c9a41f734ca5647c36d1814
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-30 10:13:13 -0500

    :arrow_up: ihmc-commons 0.34.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ mainDependencies {
    api("us.ihmc:ihmc-java-decklink-capture:0.4.0")
    api("us.ihmc:ihmc-pub-sub:0.20.0")
    api("us.ihmc:ihmc-pub-sub-serializers-extra:0.20.0")
-   api("us.ihmc:ihmc-commons:0.32.0")
+   api("us.ihmc:ihmc-commons:0.34.0")
    api("us.ihmc:ihmc-graphics-description:0.25.1")
    api("us.ihmc:mecano:17-0.18.1")
    api("com.hierynomus:sshj:0.31.0")
@@ -70,7 +70,7 @@ mainDependencies {
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.32.0")
+   api("us.ihmc:ihmc-commons-testing:0.34.0")
 }
 
 app.entrypoint("IHMCLogger", "us.ihmc.robotDataLogger.logger.YoVariableLoggerDispatcher")
