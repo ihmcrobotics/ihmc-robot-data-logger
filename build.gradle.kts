640687044b6c1e1e1082b6d2c49fc4533c9aa16a
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-30 12:30:05 -0500

    :arrow_up: mecano 17-0.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ mainDependencies {
    api("org.openjdk.jol:jol-core:0.9")
    api("org.apache.commons:commons-text:1.9")
 
-   api("us.ihmc:euclid:0.21.0")
+   api("us.ihmc:euclid:0.22.2")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:ihmc-realtime:1.6.0")
    api("us.ihmc:ihmc-java-decklink-capture:0.4.0")
@@ -45,7 +45,7 @@ mainDependencies {
    api("us.ihmc:ihmc-pub-sub-serializers-extra:0.20.0")
    api("us.ihmc:ihmc-commons:0.34.0")
    api("us.ihmc:ihmc-graphics-description:0.25.1")
-   api("us.ihmc:mecano:17-0.18.1")
+   api("us.ihmc:mecano:17-0.19.0")
    api("com.hierynomus:sshj:0.31.0")
    api("us.ihmc:scs2-definition:17-0.27.3")
 
