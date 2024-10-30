4e98ab77e90aca06d74b4350cb75b8804ffeb83a
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-30 15:37:17 -0500

    :bookmark: 0.29.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.29.9"
+   version = "0.29.10"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
