9855909b6e0f018be8f7ff7a1511eaf741da4e16
Nick Kitchel <59104880+PotatoPeeler3000@users.noreply.github.com>
2024-11-13 09:39:26 -0600

    Update LoggerDeployApplication.java

--- src/main/java/us/ihmc/publisher/logger/ui/LoggerDeployApplication.java
+++ src/main/java/us/ihmc/publisher/logger/ui/LoggerDeployApplication.java
@@ -34,7 +34,7 @@ public class LoggerDeployApplication
 
    /**
     * Helper function to open this as part of another application
-    *
+    * 
     * @param parameters Parameters from application start
     * @param scene      Parent scene
     */
@@ -119,7 +119,7 @@ public class LoggerDeployApplication
 
       String loggerDist = config.getString("loggerDist");
 
-
+      
 
       PlatformImpl.startup(new Runnable()
       {
