d5702efa123a101a6f12dfe3cd5501b46d1526a6
Duncan Calvert <dcalvert@ihmc.org>
2025-04-07 21:38:34 -0500

    Correct documentation and method name to the correct unit, microseconds.

--- src/main/java/us/ihmc/tools/CaptureTimeTools.java
+++ src/main/java/us/ihmc/tools/CaptureTimeTools.java
@@ -3,12 +3,12 @@ package us.ihmc.tools;
 public class CaptureTimeTools
 {
    /**
-    * This takes the current time in milliseconds, and returns the seconds that have passed from the start time.
+    * This takes the current time in milliseconds, and returns the microseconds that have passed from the start time.
     * @param currentTimeInMilliSeconds is the current time that you want to compare too
-    * @param startTime is the start time from when capture started
-    * @return the time in seconds that has passed since the start time
+    * @param startTime is the start time (ms) from when capture started
+    * @return the time in microseconds that has passed since the start time
     */
-   public static long timeSinceStartedCaptureInSeconds(long currentTimeInMilliSeconds, long startTime)
+   public static long timeSinceStartedCaptureInMicroseconds(long currentTimeInMilliSeconds, long startTime)
    {
       return 1000 * (currentTimeInMilliSeconds - startTime);
    }
