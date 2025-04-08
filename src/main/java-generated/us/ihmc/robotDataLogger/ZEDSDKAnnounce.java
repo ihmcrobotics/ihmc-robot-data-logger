857bf97554695519342dd326da290600dd348ad9
Duncan Calvert <dcalvert@ihmc.org>
2025-04-07 21:43:20 -0500

    Add sensor name field to ZED announce. For future use.

--- src/main/java-generated/us/ihmc/robotDataLogger/ZEDSDKAnnounce.java
+++ src/main/java-generated/us/ihmc/robotDataLogger/ZEDSDKAnnounce.java
@@ -8,11 +8,13 @@ import us.ihmc.pubsub.TopicDataType;
 
 public class ZEDSDKAnnounce extends Packet<ZEDSDKAnnounce> implements Settable<ZEDSDKAnnounce>, EpsilonComparable<ZEDSDKAnnounce>
 {
+   public java.lang.StringBuilder sensor_name_;
    public java.lang.StringBuilder address_;
    public short port_;
 
    public ZEDSDKAnnounce()
    {
+      sensor_name_ = new java.lang.StringBuilder(255);
       address_ = new java.lang.StringBuilder(255);
    }
 
@@ -24,11 +26,29 @@ public class ZEDSDKAnnounce extends Packet<ZEDSDKAnnounce> implements Settable<Z
 
    public void set(ZEDSDKAnnounce other)
    {
+      sensor_name_.setLength(0);
+      sensor_name_.append(other.sensor_name_);
+
       address_.setLength(0);
       address_.append(other.address_);
 
       port_ = other.port_;
 
+   }
+
+   public void setSensorName(java.lang.String sensor_name)
+   {
+      sensor_name_.setLength(0);
+      sensor_name_.append(sensor_name);
+   }
+
+   public java.lang.String getSensorNameAsString()
+   {
+      return getSensorName().toString();
+   }
+   public java.lang.StringBuilder getSensorName()
+   {
+      return sensor_name_;
    }
 
    public void setAddress(java.lang.String address)
@@ -73,6 +93,8 @@ public class ZEDSDKAnnounce extends Packet<ZEDSDKAnnounce> implements Settable<Z
       if(other == null) return false;
       if(other == this) return true;
 
+      if (!us.ihmc.idl.IDLTools.epsilonEqualsStringBuilder(this.sensor_name_, other.sensor_name_, epsilon)) return false;
+
       if (!us.ihmc.idl.IDLTools.epsilonEqualsStringBuilder(this.address_, other.address_, epsilon)) return false;
 
       if (!us.ihmc.idl.IDLTools.epsilonEqualsPrimitive(this.port_, other.port_, epsilon)) return false;
@@ -90,6 +112,8 @@ public class ZEDSDKAnnounce extends Packet<ZEDSDKAnnounce> implements Settable<Z
 
       ZEDSDKAnnounce otherMyClass = (ZEDSDKAnnounce) other;
 
+      if (!us.ihmc.idl.IDLTools.equals(this.sensor_name_, otherMyClass.sensor_name_)) return false;
+
       if (!us.ihmc.idl.IDLTools.equals(this.address_, otherMyClass.address_)) return false;
 
       if(this.port_ != otherMyClass.port_) return false;
@@ -104,6 +128,8 @@ public class ZEDSDKAnnounce extends Packet<ZEDSDKAnnounce> implements Settable<Z
       StringBuilder builder = new StringBuilder();
 
       builder.append("ZEDSDKAnnounce {");
+      builder.append("sensor_name=");
+      builder.append(this.sensor_name_);      builder.append(", ");
       builder.append("address=");
       builder.append(this.address_);      builder.append(", ");
       builder.append("port=");
