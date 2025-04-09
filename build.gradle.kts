a9549e9f31a94dbaa747e3ce6961abcb55c67fff
ihmc-rosie <rosie@ihmc.org>
2025-04-09 14:19:54 -0500

    :bookmark: 0.32.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.31.4"
+   version = "0.32.0"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
