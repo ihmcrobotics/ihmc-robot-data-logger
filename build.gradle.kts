f72ddeff6e9f0d042cdb34dcb03d87a35943ff36
Dexton Anderson <danderson@ihmc.org>
2025-08-29 16:51:48 -0500

    :bookmark: 0.36.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.35.0"
+   version = "0.36.0"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
