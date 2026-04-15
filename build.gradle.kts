8a97ba19893719d29ca95f4ede67db40605a8ba9
nkitchel <nkitchel@ihmc.org>
2026-04-15 13:00:01 -0500

    :up_arrow: ihmc-robot-data-logger 0.37.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.37.2"
+   version = "0.37.3"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-robot-data-logger"
    openSource = true
 
